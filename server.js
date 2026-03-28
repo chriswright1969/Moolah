d6997552496f02ad65d5fe8f09072a20c3ccebf5
chriswright1969 <81012879+chriswright1969@users.noreply.github.com>
2026-03-28 23:03:30 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import session from "express-session";
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
-import { loadContent, saveContent, getContentFilePath } from "./contentStore.js";
+import { loadContent, saveContent, getContentFilePath } from "./src/contentStore.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
